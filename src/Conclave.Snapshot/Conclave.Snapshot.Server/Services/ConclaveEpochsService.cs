bcbf13908b8ab0bcbcf2bf5758f317faf42afea3
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-17 17:45:40 +0800

    add blockfrost services

---
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Services/ConclaveEpochsService.cs
@@ -0,0 +1,25 @@
+using Blockfrost.Api.Services;
+using Conclave.Snapshot.Server.Interfaces.Services;
+using Conclave.Snapshot.Server.Models;
+
+namespace Conclave.Snapshot.Server.Services;
+
+
+public class ConclaveEpochsService : IConclaveEpochsService
+{
+    private readonly IEpochsService _service;
+
+    public ConclaveEpochsService(IEpochsService service)
+    {
+        _service = service;
+    }
+
+    public async Task<Epoch> GetCurrentEpoch(string poolId)
+    {
+        var currentEpoch = await _service.GetLatestAsync();
+
+        // check for errors before mapping
+
+        return new Epoch(currentEpoch.Epoch, new DateTime(currentEpoch.StartTime), new DateTime(currentEpoch.EndTime));
+    }
+}
