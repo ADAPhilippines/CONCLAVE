cbb6b5f2f4b6e650daa94bc3121165ebee21ae50
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-17 17:42:21 +0800

    add blockfrost & cardano services and some test codes

--- src/Conclave.Snapshot/Conclave.Snapshot.Server/Program.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Program.cs
@@ -1,11 +1,24 @@
 using Conclave.Snapshot.Server.Data;
+using Conclave.Snapshot.Server.Extensions;
+using Conclave.Snapshot.Server.Interfaces.Services;
+using Conclave.Snapshot.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddNpgsql<ApplicationDbContext>(builder.Configuration.GetValue<string>("DatabaseConfig:PostgresSQL"));
+builder.Services.AddHttpClient();
+builder.Services.AddConclaveBlockfrost(builder.Configuration);
+builder.Services.AddConclaveCardano();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (IConclaveEpochsService conclaveEpochsService) =>
+{
+    var poolId = builder.Configuration.GetValue<string>("Blockfrost:AdaPhPoolID");
+    // await service.GetPoolDelegatorsAsync(builder.Configuration.GetValue<string>("Blockfrost:AdaPhPoolID"));
+    await conclaveEpochsService.GetCurrentEpoch(poolId);
+    return "done";
+});
 
 app.Run();
