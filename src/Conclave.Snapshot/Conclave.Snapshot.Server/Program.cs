a5a6335d424586a43a5923c9b5ffe893f8f01925
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-17 12:37:45 +0800

    add database configuration and context

--- src/Conclave.Snapshot/Conclave.Snapshot.Server/Program.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Program.cs
@@ -1,5 +1,8 @@
+using Conclave.Snapshot.Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddNpgsql<ApplicationDbContext>(builder.Configuration.GetValue<string>("DatabaseConfig:PostgresSQL"));
 
 var app = builder.Build();
 
