c7b97ac56d4a9f6017744461dbb118361d5fa42e
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-18 15:59:57 +0800

    convert date kind to UTC

--- src/Conclave.Snapshot/Conclave.Snapshot.Server/Services/ConclaveEpochsService.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Services/ConclaveEpochsService.cs
@@ -1,6 +1,7 @@
 using Blockfrost.Api.Services;
 using Conclave.Snapshot.Server.Interfaces.Services;
 using Conclave.Snapshot.Server.Models;
+using Conclave.Snapshot.Server.Utils;
 
 namespace Conclave.Snapshot.Server.Services;
 
@@ -20,6 +21,8 @@ public class ConclaveEpochsService : IConclaveEpochsService
 
         // check for errors before mapping
 
-        return new Epoch(currentEpoch.Epoch, new DateTime(currentEpoch.StartTime), new DateTime(currentEpoch.EndTime));
+        return new Epoch(currentEpoch.Epoch,
+        DateUtils.UnixTimeStampToDateTime(currentEpoch.StartTime),
+        DateUtils.UnixTimeStampToDateTime(currentEpoch.EndTime));
     }
 }
