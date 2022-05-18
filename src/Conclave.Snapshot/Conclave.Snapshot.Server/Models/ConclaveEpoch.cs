4cf3828a20e40d8a02843553c499b3d8f912d9ea
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-18 14:20:53 +0800

    remove constructor for ef to work properly

--- src/Conclave.Snapshot/Conclave.Snapshot.Server/Models/ConclaveEpoch.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Models/ConclaveEpoch.cs
@@ -6,16 +6,8 @@ public class ConclaveEpoch
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public long EpochNumber { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
     public EpochStatus EpochStatus { get; set; }
 
-
-    public ConclaveEpoch(long epochNumber, DateTime startTime, DateTime endTime, EpochStatus epochStatus)
-    {
-        EpochNumber = epochNumber;
-        StartTime = startTime;
-        EndTime = endTime;
-        EpochStatus = epochStatus;
-    }
 }
