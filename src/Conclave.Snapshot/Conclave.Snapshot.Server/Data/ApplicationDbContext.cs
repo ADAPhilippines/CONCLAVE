6d7f8264eef8bc4ccb14007ccd207cfe56588e7e
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-18 14:20:35 +0800

    add tables to db context

--- src/Conclave.Snapshot/Conclave.Snapshot.Server/Data/ApplicationDbContext.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Server/Data/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
+using Conclave.Snapshot.Server.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Conclave.Snapshot.Server.Data;
 
 public class ApplicationDbContext : DbContext
 {
+
+
+
     public ApplicationDbContext(DbContextOptions options) : base(options)
     {
-        
+
     }
-}
+
+    public DbSet<ConclaveEpoch> ConclaveEpochs => Set<ConclaveEpoch>();
+    public DbSet<ConclaveSnapshot> ConclaveSnapshots => Set<ConclaveSnapshot>();
+} 
