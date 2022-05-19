f2f2602dc5e5af2af18b5ec4b6029b3f6ec2d8e9
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-19 18:42:17 +0800

    conver to pure worker service

--- src/Conclave.Snapshot/Conclave.Snapshot.Capture/Program.cs
+++ src/Conclave.Snapshot/Conclave.Snapshot.Capture/Program.cs
@@ -4,42 +4,16 @@ using Conclave.Api.Services;
 using Conclave.Server.Data;
 using Conclave.Snapshot.Capture;
 
-var builder = WebApplication.CreateBuilder(args);
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddNpgsql<ApplicationDbContext>(hostContext.Configuration.GetValue<string>("DatabaseConfig:PostgresSQL"));
+        services.AddDbContext<ApplicationDbContext>();
+        services.AddHostedService<Worker>();
+        services.AddBlockfrostServices(hostContext.Configuration);
+        services.AddConclaveApi();
+    })
+    .Build();
 
-builder.Services.AddNpgsql<ApplicationDbContext>(builder.Configuration.GetValue<string>("DatabaseConfig:PostgresSQL"));
-builder.Services.AddDbContext<ApplicationDbContext>();
-builder.Services.AddHostedService<Worker>();
-builder.Services.AddBlockfrostServices(builder.Configuration);
-builder.Services.AddConclaveApi();
+await host.RunAsync();
 
-var app = builder.Build();
-
-// app.MapGet("/", async (IConclaveEpochsService conclaveEpochsService) =>
-// {
-//     var poolId = builder.Configuration.GetValue<string>("Blockfrost:AdaPhPoolID");
-//     // await service.GetPoolDelegatorsAsync(builder.Configuration.GetValue<string>("Blockfrost:AdaPhPoolID"));
-//     await conclaveEpochsService.GetCurrentEpoch(poolId);
-//     return "done";
-// });
-
-
-// app.MapGet("/", async (IConclaveSnapshotService service) =>
-// {
-//     await service.SnapshotPoolsAsync();
-//     return "done";
-// });
-
-// app.MapGet("/", async (IConclaveSnapshotService service) =>
-// {
-//     await service.PrepareNextSnapshotCycleAsync();
-//     return "done";
-// });
-
-
-// app.MapGet("/", async (IConclaveEpochsService service) =>
-// {
-//     await service.CreateSeedEpochAsync();
-//     return "done";
-// });
-
-app.Run();
