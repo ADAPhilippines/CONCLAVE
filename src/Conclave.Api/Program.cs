a1f6958a0befe6fc594084615231cc74d78092cb
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-22 02:01:18 +0800

    add options to dependencies

--- src/Conclave.Api/Program.cs
+++ src/Conclave.Api/Program.cs
@@ -1,12 +1,14 @@
 using Conclave.Api.Extensions;
+using Conclave.Api.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ConclaveCardanoOptions>(builder.Configuration.GetSection("Conclave"));
 
 // Add services to the container.
 builder.Services.AddConclaveDb(builder.Configuration);
 builder.Services.AddBlockfrostServices(builder.Configuration);
-builder.Services.AddConclaveApi();
-
+builder.Services.AddConclaveApi(builder.Configuration);
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
