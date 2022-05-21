7cbce2987cd3698f1b0b4c01d04d8221627a0071
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-22 01:04:01 +0800

    implement some methods

--- src/Conclave.Api/Services/ConclaveSnapshotSchedulerService.cs
+++ src/Conclave.Api/Services/ConclaveSnapshotSchedulerService.cs
@@ -1,13 +1,23 @@
 using Conclave.Api.Interfaces.Services;
 using Conclave.Common.Models;
+using Conclave.Common.Utils;
 
 namespace Conclave.Api.Services;
 
 
 public class ConclaveSnapshotSchedulerService : IConclaveSnapshotSchedulerService
 {
-    public Task<long> GetSnapshotDelay(ConclaveEpoch conclaveEpoch, long delayInMilliseconds)
+    public long GetNewEpochCreationDelayInMilliseconds(ConclaveEpoch conclaveEpoch, long delayInMilliseconds)
     {
-        throw new NotImplementedException();
+        if (conclaveEpoch.EndTime is null) throw new Exception("End time not set!");
+        var millisecondDifference = (long)(conclaveEpoch.EndTime - DateUtils.DateTimeToUtc(DateTime.Now)).Value.TotalMilliseconds;
+        return millisecondDifference - delayInMilliseconds;
+    }
+
+    public long GetSnapshotDelayInMilliseconds(ConclaveEpoch conclaveEpoch, long delayInMilliseconds)
+    {
+        if (conclaveEpoch.EndTime is null) throw new Exception("End time not set!");
+        var millisecondDifference = (long)(conclaveEpoch.EndTime - DateUtils.DateTimeToUtc(DateTime.Now)).Value.TotalMilliseconds;
+        return millisecondDifference - delayInMilliseconds;
     }
 }
