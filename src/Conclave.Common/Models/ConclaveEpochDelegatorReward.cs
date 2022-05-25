00dce5a3982afc9a6f43226d119a48bf97dc555a
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-25 23:07:19 +0800

    add reward models

--- src/Conclave.Common/Models/ConclaveEpochDelegatorReward.cs
+++ src/Conclave.Common/Models/ConclaveEpochDelegatorReward.cs
@@ -8,7 +8,8 @@ public class ConclaveEpochDelegatorReward
     public Guid Id { get; set; } = Guid.NewGuid();
     public ConclaveEpochDelegator ConclaveEpochDelegator { get; set; }
     public ConclaveEpochReward ConclaveEpochReward { get; set; }
-    public float PercentageShare { get; set; }
+    public double PercentageShare { get; set; }
+    public double TokenShare { get; set; }
     public DateTime DateCreated { get; set; } = DateUtils.DateTimeToUtc(DateTime.Now);
     public DateTime DateUpdated { get; set; } = DateUtils.DateTimeToUtc(DateTime.Now);
 }
