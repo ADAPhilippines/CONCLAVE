ace0986095b1b896fa0f0bb79385864541bd68f8
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-27 13:57:47 +0800

    add helper method for calculating time difference

--- src/Conclave.Common/Utils/DateUtils.cs
+++ src/Conclave.Common/Utils/DateUtils.cs
@@ -18,4 +18,13 @@ public static class DateUtils
         dateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
         return dateTime;
     }
+
+    public static long GetTimeDifferenceFromNowInMilliseconds(DateTime? dateTime)
+    {
+        if (dateTime is null) return 0;
+
+        var utcNow = DateTime.UtcNow;
+        var difference = dateTime - utcNow;
+        return (long)difference.Value.TotalMilliseconds;
+    }
 }
