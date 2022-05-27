283e52a6d2bfc5d77b179d922666139bd245f721
johnmark83 <john.bautista@obf.ateneo.edu>
2022-05-27 13:19:40 +0800

    Fixed Code formats

--- src/Conclave.Api/Interfaces/Services/IConclaveCardanoService.cs
+++ src/Conclave.Api/Interfaces/Services/IConclaveCardanoService.cs
@@ -6,6 +6,7 @@ namespace Conclave.Api.Interfaces.Services;
 public interface IConclaveCardanoService
 {
     Task<Epoch?> GetCurrentEpochAsync();
-    Task<IEnumerable<Delegator?>> GetPoolDelegatorsAsync(string poolId, int? count = 100, int? page = 1);
+    Task<IEnumerable<Delegator>?> GetPoolDelegatorsAsync(string poolId, int? count = 100, int? page = 1);
+    Task<IEnumerable<Holder>?> GetAssetHolders(string assetAddress, int? count = 100, int? page = 1);
     Task<IEnumerable<string?>> GetAssociatedWalletAddressAsync(string stakingId);
 }
--- src/Conclave.Api/Services/ConclaveBlockfrostCardanoService.cs
+++ src/Conclave.Api/Services/ConclaveBlockfrostCardanoService.cs
@@ -12,15 +12,35 @@ public class ConclaveBlockfrostCardanoService : IConclaveCardanoService
     private readonly IEpochsService _epochsService;
     private readonly IPoolsService _poolsService;
     private readonly IAccountsService _accountsService;
+    private readonly IAssetsService _assetsService;
 
-    public ConclaveBlockfrostCardanoService(
-        IEpochsService epochsService,
-        IPoolsService poolsService,
-        IAccountsService accountsService)
+    public ConclaveBlockfrostCardanoService(IEpochsService epochsService,
+                                            IPoolsService poolsService,
+                                            IAccountsService accountsService,
+                                            IAssetsService assetsService)
     {
         _epochsService = epochsService;
         _poolsService = poolsService;
         _accountsService = accountsService;
+        _assetsService = assetsService;
+    }
+
+    public async Task<IEnumerable<Holder>?> GetAssetHolders(string assetAddress, int? count = 100, int? page = 1)
+    {
+        if (count > 100) count = 100;
+        if (count < 1) count = 100;
+        if (page < 1) page = 1;
+
+        var assetHolders = await _assetsService.GetAddressesAsync(assetAddress, count, page);
+
+        List<Holder> holders = new();
+
+        foreach (var assetHolder in assetHolders)
+        {
+            holders.Add(new Holder(assetHolder.Address, ulong.Parse(assetHolder.Quantity)));
+        }
+
+        return holders;
     }
 
     public async Task<IEnumerable<string?>> GetAssociatedWalletAddressAsync(string stakingId)
