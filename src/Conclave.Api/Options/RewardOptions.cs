54cee88a1d0127d7b8ac785d4d2aaad7ca540553
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-30 00:17:10 +0800

    add reward options

--- src/Conclave.Api/Options/RewardOptions.cs
+++ src/Conclave.Api/Options/RewardOptions.cs
@@ -5,10 +5,10 @@ public class RewardOptions
     // 73 Epoch for 1 year
     // if 1,000,000,000 total supply
     // reward is 1,3698,630 per epoch
-
-    public int MaxConclaveRewardPerEpoch { get; set; }
-    public int ConlaveRewardPerEpoch { get; set; }
-    public double PoolOwnerRewardPercentagePerEpoch { get; set; }
-    public double DelegatorRewardPercentagePerEpoch { get; set; }
-    public double NFTStakerRewardPercentagePerEpoch { get; set; }
+    public double ConclaveTokenAirdropSupply { get; set; }
+    public int ConclaveAirdropEpochsCount { get; set; }
+    public double OperatorPercentage { get; set; }
+    public double DelegatorPercentage { get; set; }
+    public double NFTPercentage { get; set; }
+    public double ConclaveOwnerRewardSharePercentage { get; set; }
 }
