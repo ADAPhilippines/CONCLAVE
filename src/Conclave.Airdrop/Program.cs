dc6653ba6a6388df78976541c4c075e798b5ee66
rjlacanlaled <rjlacanlaled@gmail.com>
2022-05-31 13:04:10 +0800

    add dependencies

--- src/Conclave.Airdrop/Program.cs
+++ src/Conclave.Airdrop/Program.cs
@@ -3,7 +3,10 @@ using Conclave.Airdrop;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        services.AddHostedService<DelegatorAirdropWorker>();
+        services.AddHostedService<NFTAirdropWorker>();
+        services.AddHostedService<OperatorAirdropWorker>();
+        services.AddHostedService<ConclaveOwnerAirdropWorker>();
     })
     .Build();
 
--- src/Conclave.Snapshot/Worker.cs
+++ src/Conclave.Snapshot/Worker.cs
@@ -72,7 +72,7 @@ public class Worker : BackgroundService
                 // prepare snapshot
                 await ExecuteSeedEpochGetterOrSetterAsync();
                 await ExecuteCurrentEpochGetterOrSetterAsync();
-                // await ExecuteSnapshotSchedulerAsync(); 
+                await ExecuteSnapshotSchedulerAsync();
                 await ExecuteNewEpochGetterOrSetterAsync();
 
                 if (NewConclaveEpoch is not null)
