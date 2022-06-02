38e9e784fd67465bb06876e7c8a8faeacacb7c7d
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-02 18:08:18 +0800

    update name to async

--- src/Conclave.Snapshot/Handlers/Snapshot/OperatorSnapshotHandler.cs
+++ src/Conclave.Snapshot/Handlers/Snapshot/OperatorSnapshotHandler.cs
@@ -11,16 +11,19 @@ public class OperatorSnapshotHandler
     private readonly IConclaveSnapshotService _snapshotService;
     private readonly IOperatorSnapshotService _operatorSnapshotService;
     private readonly IConclaveEpochsService _epochsService;
+    private readonly IDelegatorSnapshotService _delegatorSnapshotService;
     private readonly IOptions<ConclaveOptions> _options;
 
     public OperatorSnapshotHandler(IConclaveSnapshotService snapshotService,
                                    IOperatorSnapshotService operatorSnapshotService,
                                    IConclaveEpochsService epochsService,
+                                   IDelegatorSnapshotService delegatorSnapshotService,
                                    IOptions<ConclaveOptions> options)
     {
         _snapshotService = snapshotService;
         _operatorSnapshotService = operatorSnapshotService;
         _epochsService = epochsService;
+        _delegatorSnapshotService = delegatorSnapshotService;
         _options = options;
     }
 
@@ -36,7 +39,18 @@ public class OperatorSnapshotHandler
         var operatorSnapshots = await _snapshotService.SnapshotOperatorsAsync(_options.Value.PoolIds, epoch);
 
         // Save the snapshot to database
-        foreach (var operatorSnapshot in operatorSnapshots) await _operatorSnapshotService.CreateAsync(operatorSnapshot);
+        foreach (var operatorSnapshot in operatorSnapshots)
+        {
+            await _operatorSnapshotService.CreateAsync(operatorSnapshot);
+            await _delegatorSnapshotService.CreateAsync(new DelegatorSnapshot()
+            {
+                ConclaveEpoch = epoch,
+                StakeAddress = operatorSnapshot.StakeAddress,
+                WalletAddress = operatorSnapshot.WalletAddress,
+                PoolAddress = operatorSnapshot.PoolAddress,
+                Quantity = operatorSnapshot.Pledge,
+            });
+        }
 
         // Update status to Completed
         epoch.OperatorSnapshotStatus = SnapshotStatus.Completed;
