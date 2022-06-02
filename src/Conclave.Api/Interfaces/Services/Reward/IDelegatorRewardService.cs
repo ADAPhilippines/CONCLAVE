fe3e0cdd76f01b2afc4a44b5b0830634fdafba1f
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-02 18:07:39 +0800

    add stake address and epoch filter

--- src/Conclave.Api/Interfaces/Services/Reward/IDelegatorRewardService.cs
+++ src/Conclave.Api/Interfaces/Services/Reward/IDelegatorRewardService.cs
@@ -5,4 +5,6 @@ namespace Conclave.Api.Interfaces;
 public interface IDelegatorRewardService : IRepository<DelegatorReward, Guid>
 {
     IEnumerable<DelegatorReward>? GetAllByEpochNumber(ulong epochNumber);
+    IEnumerable<DelegatorReward>? GetAllByStakeAddress(string stakeAddress);
+    DelegatorReward? GetByStakeAddressAndEpochNumber(string stakeAddress, ulong epochNumber);
 }
--- src/Conclave.Api/Interfaces/Services/Reward/INFTRewardService.cs
+++ src/Conclave.Api/Interfaces/Services/Reward/INFTRewardService.cs
@@ -5,4 +5,6 @@ namespace Conclave.Api.Interfaces;
 public interface INFTRewardService : IRepository<NFTReward, Guid>
 {
     IEnumerable<NFTReward>? GetAllByEpochNumber(ulong epochNumber);
+    IEnumerable<NFTReward>? GetAllByStakeAddress(string stakeAddress);
+    IEnumerable<NFTReward>? GetAllByStakeAddressAndEpochNumber(string stakeAddress, ulong epochNumber);
 }
