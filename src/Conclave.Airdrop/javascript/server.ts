67782d5235d5f4294c26ff89f7d3e7b31470e492
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-17 17:34:25 +0800

    add todos

--- src/Conclave.Airdrop/javascript/server.ts
+++ src/Conclave.Airdrop/javascript/server.ts
@@ -1,45 +1,29 @@
-import { BlockFrostAPI } from "@blockfrost/blockfrost-js";
-import CardanoWasm from "@emurgo/cardano-serialization-lib-nodejs";
-import axios from "axios";
-import fetch from "node-fetch";
+import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
+import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
+import axios from 'axios';
+import fetch from 'node-fetch';
+
+// TODO: connect to database
 
 function harden(num: number): number {
-  return 0x80000000 + num;
+    return 0x80000000 + num;
 }
 
 const blockfrostAPI = new BlockFrostAPI({
-  projectId: process.env.PROJECT_ID as string,
+    projectId: process.env.PROJECT_ID as string,
 });
 
-var mnemonic = [
-  "energy",
-  "crater",
-  "shallow",
-  "must",
-  "bronze",
-  "clog",
-  "level",
-  "velvet",
-  "such",
-  "trial",
-  "increase",
-  "liberty",
-];
-
 // instantiate the tx builder with the Cardano protocol parameters - these may change later on
-const linearFee = CardanoWasm.LinearFee.new(
-  CardanoWasm.BigNum.from_str("44"),
-  CardanoWasm.BigNum.from_str("155381")
-);
+const linearFee = CardanoWasm.LinearFee.new(CardanoWasm.BigNum.from_str('44'), CardanoWasm.BigNum.from_str('155381'));
 
 const txBuilderCfg = CardanoWasm.TransactionBuilderConfigBuilder.new()
-  .fee_algo(linearFee)
-  .pool_deposit(CardanoWasm.BigNum.from_str("500000000"))
-  .key_deposit(CardanoWasm.BigNum.from_str("2000000"))
-  .max_value_size(4000)
-  .max_tx_size(8000)
-  .coins_per_utxo_word(CardanoWasm.BigNum.from_str("34482"))
-  .build();
+    .fee_algo(linearFee)
+    .pool_deposit(CardanoWasm.BigNum.from_str('500000000'))
+    .key_deposit(CardanoWasm.BigNum.from_str('2000000'))
+    .max_value_size(4000)
+    .max_tx_size(8000)
+    .coins_per_utxo_word(CardanoWasm.BigNum.from_str('34482'))
+    .build();
 
 const txBuilder = CardanoWasm.TransactionBuilder.new(txBuilderCfg);
 
