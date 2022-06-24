37c3dddc2412b23adabd8fcba25ed09dd0197a5d
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-24 10:45:33 +0800

    add main method

--- src/Conclave.Airdrop/javascript/server.ts
+++ src/Conclave.Airdrop/javascript/server.ts
@@ -2,30 +2,17 @@ import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
 import axios from 'axios';
 import fetch from 'node-fetch';
-
-// TODO: connect to database
-
-function harden(num: number): number {
-    return 0x80000000 + num;
-}
+import { getAllUnpaidAdaRewardsAsync, getAllUnpaidConclaveTokenRewardsAsync } from './utils/reward_utils';
 
 const blockfrostAPI = new BlockFrostAPI({
     projectId: process.env.PROJECT_ID as string,
 });
 
-// instantiate the tx builder with the Cardano protocol parameters - these may change later on
-const linearFee = CardanoWasm.LinearFee.new(CardanoWasm.BigNum.from_str('44'), CardanoWasm.BigNum.from_str('155381'));
+const main = async () => {
+    const unpaidList = await getAllUnpaidConclaveTokenRewardsAsync();
+    unpaidList.forEach((reward) => {
+        console.log(reward);
+    });
+};
 
-const txBuilderCfg = CardanoWasm.TransactionBuilderConfigBuilder.new()
-    .fee_algo(linearFee)
-    .pool_deposit(CardanoWasm.BigNum.from_str('500000000'))
-    .key_deposit(CardanoWasm.BigNum.from_str('2000000'))
-    .max_value_size(4000)
-    .max_tx_size(8000)
-    .coins_per_utxo_word(CardanoWasm.BigNum.from_str('34482'))
-    .build();
-
-const txBuilder = CardanoWasm.TransactionBuilder.new(txBuilderCfg);
-
-console.log(process.env.BASE_ADDRESS);
-console.log(process.env.PROJECT_ID);
+main();
