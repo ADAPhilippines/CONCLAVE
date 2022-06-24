0ec1c923f5f9f067b7826c50faeb9d833668ddcf
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-24 15:09:48 +0800

    add delegator snapshot property

--- src/Conclave.Common/Models/Entities/OperatorSnapshot.cs
+++ src/Conclave.Common/Models/Entities/OperatorSnapshot.cs
@@ -6,6 +6,7 @@ namespace Conclave.Common.Models;
 public class OperatorSnapshot
 {
     public Guid Id { get; set; } = Guid.NewGuid();
+    public DelegatorSnapshot DelegatorSnapshot { get; set; } = new();
     public ConclaveEpoch ConclaveEpoch { get; set; } = new();
     public string PoolAddress { get; set; } = string.Empty;
     public string StakeAddress { get; set; } = string.Empty;
