8cdd4756a6d9b2200d6cf220b65f1cc3d6c24efa
johnmark83 <john.bautista@obf.ateneo.edu>
2022-06-26 18:45:59 +0800

    Added AirdropStatus Edit

--- src/Conclave.Airdrop/javascript/server.ts
+++ src/Conclave.Airdrop/javascript/server.ts
@@ -2,19 +2,26 @@
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
 import axios from 'axios';
+import { mnemonicToEntropy } from 'bip39';
 import fetch from 'node-fetch';
-import { getAllUnpaidAdaRewardsAsync, getAllUnpaidConclaveTokenRewardsAsync } from './utils/reward_utils';
+import { getAllUnpaidAdaRewardsAsync, getAllUnpaidConclaveTokenRewardsAsync } from './utils/reward-utils';
+import { fromHex } from './utils/string-utils';
+import { handleTransactionAsync } from './utils/transaction-utils';
+import cbor from 'cbor';
 
 const blockfrostAPI = new BlockFrostAPI({
     projectId: process.env.PROJECT_ID as string,
-    isTestnet: true
+    isTestnet: false
 });
 
 const main = async () => {
-    const unpaidList = await getAllUnpaidConclaveTokenRewardsAsync();
-    unpaidList.forEach((reward) => {
-        console.log(reward);
-    });
+    // const unpaidList = await getAllUnpaidConclaveTokenRewardsAsync();
+    // unpaidList.forEach((reward) => {
+    //     console.log(reward);
+    // });
+    // await handleTransactionAsync(unpaidList);
+    await handleTransactionAsync();
 };
 
 main();
+// handleTransactionAsync();
