2827830d336fc7a6fe35dbff32e2709c549ff46f
rjlacanlaled <rjlacanlaled@gmail.com>
2022-06-28 13:47:26 +0800

    test utxo fetcher

--- src/Conclave.Airdrop/javascript/server.ts
+++ src/Conclave.Airdrop/javascript/server.ts
@@ -1,13 +1,13 @@
-
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 import CardanoWasm from '@emurgo/cardano-serialization-lib-nodejs';
 import axios from 'axios';
 import fetch from 'node-fetch';
-import { getAllUnpaidAdaRewardsAsync, getAllUnpaidConclaveTokenRewardsAsync } from './utils/reward_utils';
+import { getAllUnpaidAdaRewardsAsync, getAllUnpaidConclaveTokenRewardsAsync } from './utils/reward-utils';
+import { getUtxosWithAsset } from './utils/utxo-utils';
 
 const blockfrostAPI = new BlockFrostAPI({
     projectId: process.env.PROJECT_ID as string,
-    isTestnet: true
+    isTestnet: true,
 });
 
 const main = async () => {
@@ -15,6 +15,14 @@ const main = async () => {
     unpaidList.forEach((reward) => {
         console.log(reward);
     });
+
+    const utxosWithAsset = await getUtxosWithAsset(
+        blockfrostAPI,
+        process.env.BASE_ADDRESS as string,
+        '6b8d07d69639e9413dd637a1a815a7323c69c86abbafb66dbfdb1aa7'
+    );
+
+    console.log({ utxosWithAsset });
 };
 
 main();
