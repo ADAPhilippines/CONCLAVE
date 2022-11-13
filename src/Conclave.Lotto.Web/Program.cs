5696a142ff363aef6fef02b09b07bb1b6843c0e3
Clark Alesna <clark_alesna@hotmail.com>
2022-11-13 20:56:23 +0800

    Added Nethereum Metamask Example /test page

--- src/Conclave.Lotto.Web/Program.cs
+++ src/Conclave.Lotto.Web/Program.cs
@@ -4,12 +4,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Conclave.Lotto.Web;
 using MudBlazor.Services;
 using Conclave.Lotto.Web.Services;
+using Nethereum.Metamask;
+using Nethereum.Metamask.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<IMetamaskInterop, MetamaskBlazorInterop>();
+builder.Services.AddSingleton<MetamaskInterceptor>();
+builder.Services.AddSingleton<MetamaskHostProvider>();
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped<DataService>();
