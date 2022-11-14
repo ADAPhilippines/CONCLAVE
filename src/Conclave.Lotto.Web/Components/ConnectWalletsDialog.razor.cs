c45b409c157bb64f775499e4a84d611d8ce72517
Alphonzo Escolar <phonz.dev@gmail.com>
2022-11-14 21:52:20 +0800

    Integrated CardanoWalletsDialog component (work in progress)

--- src/Conclave.Lotto.Web/Components/ConnectWalletsDialog.razor.cs
+++ src/Conclave.Lotto.Web/Components/ConnectWalletsDialog.razor.cs
@@ -5,9 +5,15 @@ namespace Conclave.Lotto.Web.Components;
 
 public partial class ConnectWalletsDialog
 {
-    [Inject] IDialogService? DialogService { get; set; } = default;
+    [Inject] IDialogService DialogService { get; set; } = default!;
 
     [CascadingParameter] MudDialogInstance MudDialog { get; set; } = default!;
+
+    private void OpenCardanoWalletsDialog()
+    {
+        var options = new DialogOptions { CloseOnEscapeKey = true };
+        DialogService.Show<CardanoWalletsDialog>("", options);
+    }
 
     void Submit() => MudDialog.Close(DialogResult.Ok(true));
     void Cancel() => MudDialog.Cancel();
