0f8426096170d0a2a2a9269bf596fed7b913859b
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-14 16:42:12 +0800

    refactor properties

--- src/Conclave.Oracle.Contract/fixtures/token.ts
+++ src/Conclave.Oracle.Contract/fixtures/token.ts
@@ -17,39 +17,51 @@ export async function deployTokenFixture() {
     const token = await Token.deploy(name, ticker, totalSupply);
     await token.deployed();
     const decimal = await token.decimals();
-    console.log(chalk.green(`Token deployed to: ${chalk.blue(token.address)}\n\n`));
+    console.log(chalk.green(`Token successfully deployed to: ${chalk.blue(token.address)}\n\n`));
 
     return { token, decimal, name, ticker, totalSupply };
 }
 
 export async function airdropTokenFixture() {
-    let _token, _decimal;
+    let token, decimal, name, ticker, totalSupply;
 
     const network = await ethers.provider.getNetwork();
 
     if (network.chainId === hardhatConfig.networks?.hardhat?.chainId) {
-        const { token, decimal } = await deployTokenFixture();
-        _token = token;
-        _decimal = decimal;
+        const {
+            token: _token,
+            decimal: _decimal,
+            name: _name,
+            ticker: _ticker,
+            totalSupply: _totalSupply,
+        } = await deployTokenFixture();
+        token = _token;
+        decimal = _decimal;
+        name = _name;
+        ticker = _ticker;
+        totalSupply = _totalSupply;
     } else {
         try {
-            _token = await ethers.getContractAt('Token', config.tokenAddress);
-            _decimal = await _token.decimals();
+            token = await ethers.getContractAt('Token', config.tokenAddress);
+            decimal = await token.decimals();
+            name = await token.name();
+            ticker = await token.symbol();
+            totalSupply = await token.totalSupply();
         } catch (err) {
             console.log(chalk.red('Token contract not found on this network.'));
             process.exit(1);
         }
     }
 
-    const tokenAmount = ethers.utils.parseUnits('100000', _decimal);
+    const tokenAmount = ethers.utils.parseUnits('100000', decimal);
     const accounts = await ethers.getSigners();
 
     console.log(chalk.yellow(`Airdropping tokens to ${chalk.blue(accounts.length)} accounts`));
     for (const account of accounts) {
-        await _token.transfer(account.address, tokenAmount);
+        await token.transfer(account.address, tokenAmount);
         console.log(
             chalk.green(
-                `Airdropped ${chalk.blue(ethers.utils.formatUnits(tokenAmount, _decimal))} to ${chalk.blue(
+                `Airdropped ${chalk.blue(ethers.utils.formatUnits(tokenAmount, decimal))} to ${chalk.blue(
                     account.address
                 )}`
             )
@@ -57,4 +69,5 @@ export async function airdropTokenFixture() {
     }
 
     console.log('\n\n');
+    return { token, decimal, name, ticker, totalSupply };
 }
