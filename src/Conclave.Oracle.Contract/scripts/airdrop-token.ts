1e7cb2301f0b5856a5e8f3232914b45e8fa81ce6
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-14 18:35:49 +0800

    update script to only milkomeda

--- src/Conclave.Oracle.Contract/scripts/airdrop-token.ts
+++ src/Conclave.Oracle.Contract/scripts/airdrop-token.ts
@@ -1,7 +1,33 @@
-import { airdropTokenFixture } from '../fixtures/token';
+import chalk from 'chalk';
+import { ethers } from 'hardhat';
+import config from '../config.json';
+import { token } from '../typechain-types/@openzeppelin/contracts';
 
 async function main() {
-    await airdropTokenFixture();
+    try {
+        const token = await ethers.getContractAt('Token', config.tokenAddress);
+        const decimal = await token.decimals();
+
+        const tokenAmount = ethers.utils.parseUnits('100000', decimal);
+        const accounts = await ethers.getSigners();
+
+        console.log(chalk.yellow(`Airdropping tokens to ${chalk.blue(accounts.length)} accounts`));
+        for (const account of accounts) {
+            await token.transfer(account.address, tokenAmount);
+            console.log(
+                chalk.green(
+                    `Airdropped ${chalk.blue(ethers.utils.formatUnits(tokenAmount, decimal))} to ${chalk.blue(
+                        account.address
+                    )}`
+                )
+            );
+        }
+
+        console.log('\n\n');
+    } catch (err) {
+        console.log(chalk.red('Token contract not found on this network.'));
+        process.exit(1);
+    }
 }
 
 main();
