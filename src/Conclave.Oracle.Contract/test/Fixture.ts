13e4a7d74f4b1b32fa80b90e6525e32e130d657c
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-15 18:48:11 +0800

    separate delegation fixture to operator fixture

--- src/Conclave.Oracle.Contract/test/Fixture.ts
+++ src/Conclave.Oracle.Contract/test/Fixture.ts
@@ -1,45 +1,44 @@
 import { ethers } from 'hardhat';
 import { BigNumber } from 'ethers';
 import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import chalk from 'chalk';
 
 export default async function fixture() {
-    const { token, decimal, oracle, accounts, accountsWithTokens, accountsWithoutTokens, minValidatorStake } = await stakingFixture();
+    const {
+        token,
+        decimal,
+        oracle,
+        accounts,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        minValidatorStake,
+        consumer,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+        stakeAndDelegate,
+        getRandomStakeAmount,
+    } = await operatorFixture();
 
-    // deploy consumer
-    const Consumer = await ethers.getContractFactory('OracleConsumer');
-    const consumer = await Consumer.deploy(oracle.address, token.address);
-
-    // transfer assets to consumer
-    const ethAmount = ethers.utils.parseEther('10');
-    const cnclvAmount = ethers.utils.parseUnits('1000000', decimal);
-
-    await accounts[0].sendTransaction({
-        to: consumer.address,
-        value: ethAmount,
-    });
-
-    token.transfer(consumer.address, cnclvAmount);
-
-    // approvals
-    await consumer.approve();
-    for (const account of accountsWithTokens) {
-        await token.connect(account).approve(oracle.address, ethers.constants.MaxUint256);
-    }
-
-    // stake nodes
-    for (const account of accounts) {
-        const minStake = ethers.utils.formatUnits((await oracle.s_minStake()).toString(), decimal);
-        const maxStake = ethers.utils.formatUnits(await token.balanceOf(account.address), decimal);
-        const amount = ethers.utils.parseUnits((Math.random() * (Number(maxStake) - Number(minStake)) + Number(minStake)).toString(), decimal);
-        await oracle.connect(account).stake(amount);
-    }
-
-    // delegate nodes
-    for (const account of accounts) {
-        await oracle.connect(account).delegateNode(account.address);
-    }
-
-    return { token, decimal, oracle, accounts, accountsWithTokens, accountsWithoutTokens, minValidatorStake, consumer };
+    return {
+        token,
+        decimal,
+        oracle,
+        accounts,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        minValidatorStake,
+        consumer,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+        stakeAndDelegate,
+        getRandomStakeAmount,
+    };
 }
 
 export async function stakingFixture() {
@@ -65,7 +64,7 @@ export async function stakingFixture() {
     // deploy oracle
     const minValidatorStake = ethers.utils.parseUnits('10000', decimal);
     const jobAcceptanceLimitInSeconds = 60; // 1 minute
-    const jobFulFillmentLimitInSeconds = 60; // 1 minute per Number
+    const jobFulFillmentLimitInSeconds = 60; // 1 minute
     const slashingAmount = ethers.utils.parseUnits('1000', decimal);
     const minAdaStakingRewards = ethers.utils.parseEther('10');
     const minTokenStakingRewards = ethers.utils.parseUnits('10000', decimal);
@@ -115,5 +114,159 @@ export async function stakingFixture() {
         approve,
         approveAndStake,
         unstake,
+    };
+}
+
+export async function delegateNodeFixture() {
+    const {
+        token,
+        decimal,
+        oracle,
+        accounts,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        minValidatorStake,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+    } = await stakingFixture();
+
+    const stakeAndDelegate = async (operator: SignerWithAddress, node: SignerWithAddress, amount: BigNumber) => {
+        await approveAndStake(operator, amount);
+        await oracle.connect(operator).delegateNode(node.address);
+    };
+
+    const getRandomStakeAmount = (min: number, max: number): BigNumber => {
+        const amount = Math.random() * (max - min) + min;
+        return ethers.utils.parseUnits(amount.toString(), decimal);
+    };
+
+    return {
+        token,
+        decimal,
+        oracle,
+        accounts,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        minValidatorStake,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+        stakeAndDelegate,
+        getRandomStakeAmount,
+    };
+}
+
+export async function operatorFixture() {
+    const {
+        oracle,
+        accounts,
+        token,
+        decimal,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        stakeAndDelegate,
+        getRandomStakeAmount,
+        minValidatorStake,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+    } = await delegateNodeFixture();
+
+    // deploy consumer
+    const Consumer = await ethers.getContractFactory('OracleConsumer');
+    const consumer = await Consumer.deploy(oracle.address, token.address);
+
+    // transfer assets to consumer
+    const ethAmount = ethers.utils.parseEther('100');
+    const cnclvAmount = ethers.utils.parseUnits('1000000', decimal);
+
+    await accounts[0].sendTransaction({
+        to: consumer.address,
+        value: ethAmount,
+    });
+
+    token.transfer(consumer.address, cnclvAmount);
+
+    // approvals
+    await consumer.approve();
+    for (const account of accountsWithTokens) {
+        await token.connect(account).approve(oracle.address, ethers.constants.MaxUint256);
+    }
+
+    const adaFee = ethers.utils.parseEther('0.5');
+    const adaFeePerNum = ethers.utils.parseEther('0.1');
+    const tokenFee = ethers.utils.parseUnits('1000', decimal);
+    const tokenFeePerNum = ethers.utils.parseUnits('100', decimal);
+    const minValidator = ethers.BigNumber.from('1');
+    const maxValidator = ethers.BigNumber.from('1');
+    const numCount = 1;
+    const total = adaFee.add(adaFeePerNum.mul(numCount));
+
+    const operators = accountsWithTokens;
+    const nodes = accountsWithoutTokens;
+
+    // stake and delegate
+    let index = 0;
+    for (const operator of operators) {
+        const formattedMinValidatorStake = ethers.utils.formatUnits(minValidatorStake, decimal);
+        const formattedBalance = ethers.utils.formatUnits(await token.balanceOf(operator.address), decimal);
+        const randomStakeAmount = getRandomStakeAmount(Number(formattedMinValidatorStake), Number(formattedBalance));
+        await stakeAndDelegate(operator, nodes[index], randomStakeAmount);
+
+        index++;
+    }
+
+    const tx = await consumer.requestRandomNumbers(numCount, adaFee, adaFeePerNum, tokenFee, tokenFeePerNum, minValidator, maxValidator, {
+        value: total,
+    });
+    const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+    const data = receipt.logs[1].data;
+    const topics = receipt.logs[1].topics;
+    const jobRequestEvent = new ethers.utils.Interface(['event JobRequestCreated(uint256 jobId,uint32 indexed numCount,uint256 indexed timestamp)']);
+
+    const event = jobRequestEvent.decodeEventLog('JobRequestCreated', data, topics);
+    const sampleRequestId = event.jobId;
+
+    const getResponse = (count: number) => {
+        const res = [];
+        for (let i = 0; i < count; i++) {
+            res.push(ethers.utils.randomBytes(32));
+        }
+        return res;
+    };
+
+    return {
+        token,
+        decimal,
+        oracle,
+        accounts,
+        accountsWithTokens,
+        accountsWithoutTokens,
+        minValidatorStake,
+        consumer,
+        testStakeAmount,
+        stake,
+        approve,
+        approveAndStake,
+        unstake,
+        stakeAndDelegate,
+        getRandomStakeAmount,
+        adaFee,
+        adaFeePerNum,
+        tokenFee,
+        tokenFeePerNum,
+        minValidator,
+        maxValidator,
+        sampleRequestId,
+        operators,
+        nodes,
+        getResponse,
     };
 }
