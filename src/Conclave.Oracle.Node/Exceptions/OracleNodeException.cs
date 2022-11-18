2d1f2207fce75b5cd9d52ee40aec9ad5020dbb31
johnmark83 <john.bautista@obf.ateneo.edu>
2022-11-18 12:11:27 +0800

    deleted unnecessary files

--- src/Conclave.Oracle.Node/Exceptions/OracleNodeException.cs
+++
@@ -1,8 +0,0 @@
-namespace Conclave.Oracle.Node.Exceptions;
-
-public class OracleNodeException : Exception
-{
-    public OracleNodeException() : base() { }
-    public OracleNodeException(string message) : base(message) { }
-    public OracleNodeException(string message, Exception inner) : base(message, inner) { }
-}
