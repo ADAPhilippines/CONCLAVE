de71cefcf98e00d216ec2749fa9f03c5569e0e18
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-19 16:05:20 +0800

    add helper functions

--- src/Conclave.Oracle.Contract/contracts/interfaces/IConclaveOracleOperator.sol
+++ src/Conclave.Oracle.Contract/contracts/interfaces/IConclaveOracleOperator.sol
@@ -2,18 +2,47 @@
 pragma solidity ^0.8.17;
 
 interface IConclaveOracleOperator {
+    struct JobRequest {
+        uint256 jobId;
+        uint256 baseAdaFee;
+        uint256 baseTokenFee;
+        uint256 adaFeePerNum;
+        uint256 tokenFeePerNum;
+        uint256 timestamp;
+        uint256 jobAcceptanceExpiration;
+        uint256 jobFulfillmentExpiration;
+        uint256 finalResultDataId;
+        uint24 responseCount;
+        uint24 numCount;
+        uint24 minValidator;
+        uint24 maxValidator;
+        address requester;
+        address[] validators;
+        uint256[] dataIds;
+        RequestStatus status;
+    }
+
+    enum RequestStatus {
+        Pending,
+        Refunded,
+        Fulfilled
+    }
+
     function delegateNode(address node) external;
+
+    function setNodeAllowance(uint24 allowance) external;
 
     function acceptJob(uint256 jobId) external;
 
     function submitResponse(uint256 jobIb, uint256[] calldata response)
         external;
 
-    function isJobReady(uint256 jobId) external view returns (bool);
+    function getJobDetails(uint256 jobId)
+        external
+        view
+        returns (JobRequest memory);
 
-    function isResponseSubmitted(uint256 jobId) external view returns (bool);
-
-    function getPendingRewards(uint256 jobId)
+    function getPendingRewardsByJobId(uint256 jobId)
         external
         view
         returns (uint256 reward, uint256 tokenReward);
@@ -23,5 +52,22 @@ interface IConclaveOracleOperator {
         view
         returns (uint256 reward, uint256 tokenReward);
 
+    function getOwner(address node) external view returns (address owner);
+
+    function getNode(address owner) external view returns (address node);
+
+    function getNodeAllowance(address node) external view returns (uint256);
+
+    function getPendingRewardJobIds(address node)
+        external
+        view
+        returns (uint256[] memory);
+
     function claimPendingRewards() external;
+
+    function isJobReady(uint256 jobId) external view returns (bool);
+
+    function isResponseSubmitted(uint256 jobId) external view returns (bool);
+
+    function isNodeRegistered(address node) external view returns (bool);
 }
