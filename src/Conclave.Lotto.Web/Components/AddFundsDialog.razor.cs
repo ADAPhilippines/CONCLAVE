ab1003de762dbdccbf323b7de6f4da9962b03459
Alphonzo Escolar <phonz.dev@gmail.com>
2022-11-22 14:37:54 +0800

    Modified AddFundsDialog (WIP)

--- src/Conclave.Lotto.Web/Components/AddFundsDialog.razor.cs
+++ src/Conclave.Lotto.Web/Components/AddFundsDialog.razor.cs
@@ -6,21 +6,21 @@ namespace Conclave.Lotto.Web.Components;
 
 public partial class AddFundsDialog
 {
-    [CascadingParameter]
-    MudDialogInstance? MudDialog { get; set; }
+    // [CascadingParameter]
+    // MudDialogInstance? MudDialog { get; set; }
 
-    public int TextValue { get; set; }
+    // public int TextValue { get; set; }
 
-    private void OnBtnDepositClicked()
-    {
-        if (MudDialog is not null)
-            MudDialog.Close(DialogResult.Ok(true));
-    }
+    // private void OnBtnDepositClicked()
+    // {
+    //     if (MudDialog is not null)
+    //         MudDialog.Close(DialogResult.Ok(true));
+    // }
 
-    private void OnBtnCancelClicked()
-    {
-        if (MudDialog is not null) MudDialog.Cancel();
-    }
+    // private void OnBtnCancelClicked()
+    // {
+    //     if (MudDialog is not null) MudDialog.Cancel();
+    // }
 
     private void OnBtnMaxClicked() => Console.WriteLine("MaxClicked");
 }
