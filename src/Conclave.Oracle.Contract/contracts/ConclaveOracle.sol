4261cc1100dd97cd9669e351d0ee486507b0ce76
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-23 00:09:15 +0800

    remove all logs from contract

--- src/Conclave.Oracle.Contract/contracts/ConclaveOracle.sol
+++ src/Conclave.Oracle.Contract/contracts/ConclaveOracle.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.17;
 
 import "./abstracts/ConclaveOracleOperator.sol";
 import "./interfaces/IConclaveOracle.sol";
-import "hardhat/console.sol";
 
 contract ConclaveOracle is IConclaveOracle, ConclaveOracleOperator {
     error ValueMismatch(uint256 stated, uint256 actual);
